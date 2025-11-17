51e8c23f73bdda888aa2be8ac595af145f12470c
dharshiniramu <dharshiniramu78@gmail.com>
2025-11-17 16:19:19 +0530

    modified changes for d4

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,7 @@
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 // Removed PageTransition for better performance
